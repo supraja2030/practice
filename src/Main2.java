844c3e30f81a32034ebc95dc5bce13fcee653c47
supraja2030 <bsupraja7891@gmail.com>
2024-06-13 14:48:02 +0530

    methods

---
+++ src/Main2.java
@@ -0,0 +1,7 @@
+public class Main2 {
+    public static void main(String[] args){
+        Vehicle2 vehicle = new Vehicle2();
+        vehicle.start();
+        vehicle.stop();
+    }
+}
---
+++ src/Vehicle2.java
@@ -0,0 +1,9 @@
+public class Vehicle2 {
+    public void stop(){
+        System.out.println("The vehicle has been stopped ");
+    }
+    public void start(){
+        System.out.println("The vehicle is moving ");
+    }
+
+}
