62927d237fb709fd2ca28431461c4e4ab72c92f9
supraja2030 <bsupraja7891@gmail.com>
2024-06-14 12:55:34 +0530

    polymorphism method overriding

---
+++ src/Main10.java
@@ -0,0 +1,13 @@
+public class Main10 {
+    public static void main(String[] args){
+
+        Animal animal;
+
+        animal = new Dog();
+        animal.sound();
+
+        animal = new Cat();
+        animal.sound();
+
+    }
+}
