8c3d929e261c308cff3e24466a87cc3ef3029510
supraja2030 <bsupraja7891@gmail.com>
2024-06-14 11:35:35 +0530

    packages

---
+++ src/tools/Car.java
@@ -0,0 +1,12 @@
+package tools;
+
+public class Car {
+    public void stop(){
+        System.out.println("The car has been stopped");
+    }
+    public void start(){
+        System.out.println("The car has been started ");
+    }
+
+
+}
---
+++ src/tools/Main.java
@@ -0,0 +1,10 @@
+import tools.Car;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.start();
+        car.stop();
+
+    }
+}
