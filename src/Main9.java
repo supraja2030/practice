fa2d00334607d2656aa7a40b066998e0beb0d39d
supraja2030 <bsupraja7891@gmail.com>
2024-06-14 12:43:47 +0530

    polymorphism method overloading

---
+++ src/Main9.java
@@ -0,0 +1,11 @@
+public class Main9 {
+    public static void main(String[] args){
+        mathOperations math = new mathOperations();
+
+        System.out.println(math.add(10,20));
+        System.out.println(math.add(10.2,5.6));
+        System.out.println(math.add(5,5,5));
+    }
+
+}
+
