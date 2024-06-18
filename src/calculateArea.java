bf8fb048b517c3e5414008ef7a41b22ddcf58ad1
supraja2030 <bsupraja7891@gmail.com>
2024-06-18 10:43:30 +0530

    calculate area and perimeter

---
+++ src/calculateArea.java
@@ -0,0 +1,9 @@
+public class calculateArea {
+    public int areaOfRectangle(int a, int b) {
+
+        return a*b;
+    }
+    public int PerimeterOfRectangle(int a, int b ){
+        return 2*(a+b);
+    }
+}
---
+++ src/shape.java
@@ -0,0 +1,15 @@
+public class shape {
+    public static void main(String[] args) {
+        calculateArea method = new calculateArea();
+        int x = method.areaOfRectangle(10,20);
+        int y = method.PerimeterOfRectangle(10,20);
+        System.out.println(x);
+        System.out.println(y);
+
+
+
+
+
+
+    }
+}
